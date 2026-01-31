e244a447a221e3cc38befb7228aa55fd3f64af64
Sagnik Chakraborty <sagnikchakraborty368@gmail.com>
2026-01-31 20:17:26 +0530

    fucked up

--- final.py
+++ final.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -8,3 +9,5 @@ from sklearn.metrics import r2_score
 df = pd.read_csv("synthetic_mental_health_dataset.csv")
 df.info()
 df.describe()
+df
+sns.kdeplot(df['sleep_hours'])
